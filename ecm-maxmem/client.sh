30253b057e2c373cbda6d446aa2c3ca347497bec
Seth Troisi <sethtroisi@google.com>
2019-05-30 22:05:39 -0700

    With stats and better names

--- ecm-maxmem/client.sh
+++ ecm-maxmem/client.sh
@@ -2,13 +2,13 @@
 
 sudo apt install -y gmp-ecm
 
-function() run_and_log() {
+function run_and_log() {
     method=$1
     b1=$2
     maxmem=$3
     log_file=$4
 
-    echo "time ecm -v $method -maxmem=$maxmem $b1 << \"prime_$i.txt\""
+    echo "time ecm -v $method -maxmem=$maxmem $b1 > \"prime_$i.txt\""
     time ecm -v $method -maxmem $maxmem $b1 < "prime_$i.txt" |& tee -a $log_file
 
     gsutil cp $log_file gs://cloudy-go/ecm-maxmem
@@ -16,18 +16,20 @@ function() run_and_log() {
 
 for i in `seq 1 8`; do
     [[ -f "prime_$i.txt" ]] || continue
-    for b1 in 100e6 1e9 10e9; do
-        log_file="ecm_${i}_${b1}_maxmem_0.log"
 
-        run_and_log "-pm1" "$b1" 16000 "$log_file"
+    psize=$(($(cat "prime_$i.txt" | wc -c)-1))
+
+    for b1 in 100e6 300e6; do #1e9 3e9; do
+        log_file="ecm_p${psize}_${b1}_maxmem"
+
+        run_and_log "-pm1" "$b1" 16000 "${log_file}_0.log"
 
         # Run with less memory
         usage=$(grep 'Peak memory usage:' $log_file | grep -o '[0-9]\+')
         for d in 2 3 5 10 20; do
             memory=$(( (usage + 10) / d ))
-            log_file="ecm_${i}_${b1}_maxmem_${d}_${memory}MB.log"
 
-            run_and_log "-pm1" "$b1" "$memory" "$log_file"
+            run_and_log "-pm1" "$b1" "$memory" "${log_file}_${memory}MB.log"
         done
     done
 done
--- ecm-maxmem/run.sh
+++ ecm-maxmem/run.sh
@@ -20,12 +20,16 @@ echo
 echo "screen -S maxmem -t 0 -d -m"
 
 for i in `seq 1 8`; do
-    echo "screen -S maxmem -X screen -t $i"
-#    echo "screen -S maxmem -p $i -X stuff $'gcloud compute --project "cloudy-go" ssh --zone "us-west1-b" "gmp-ecm-maxmem-test-"$i\\\r'"
     prime=$(cat primes.txt | sed -n "${i}p")
     echo $prime > "prime_$i.txt"
     echo gcloud compute scp --zone "us-west1-b" client.sh prime_$i.txt "gmp-ecm-maxmem-test-"$i:~/
+    echo "screen -S maxmem -X screen -t $i"
     echo "screen -S maxmem -p $i -X stuff $'gcloud compute ssh --zone "us-west1-b" "gmp-ecm-maxmem-test-"$i\\\r'"
 done
 
 echo
+echo "Stats:"
+echo
+echo "mkdir -p results"
+echo "gsutil -m cp gs://cloudy-go/ecm-maxmem/* results/"
+echo 'grep "Step . took\|Peak memory" $(find results/ | sort -n -t_ -k1,5)'
