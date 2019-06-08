798707bee9df5bd9b7bbc3c5dbce7623b32470e4
Seth Troisi <sethtroisi@google.com>
2019-06-07 17:24:26 -0700

    Added gcp-screen-startup script for cado-nfs-client

---
+++ cado-nfs/gcp-screen-startup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+processes="$1"
+server="$2"
+
+
+cd_dir="cd Math/cado-nfs"
+run_cado="./cado-nfs-client.py --override t 8 --bindir=build/\$(hostname) --server=$server"
+
+echo "screen -S cado-nfs-client -t cado -d -m"
+echo "screen -S cado-nfs-client -p 0 -X stuff $'htop\\\r'"
+
+for i in `seq 1 $processes`; do
+    echo "screen -S cado-nfs-client -X screen -t cado-$i"
+    echo "screen -S cado-nfs-client -p $i -X stuff $'$cd_dir; time $run_cado \\\r'"
+done
