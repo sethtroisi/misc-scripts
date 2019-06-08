112b7c0c7c35916f4d448bed2463d3b9267d7993
Seth Troisi <sethtroisi@google.com>
2019-06-07 18:23:39 -0700

    Fix stuff

--- cado-nfs/gcp-screen-startup.sh
+++ cado-nfs/gcp-screen-startup.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-processes="$(($(cat /proc/cpuinfo  | grep proces | wc -l)/4 + 1))"
+processes="$(($(cat /proc/cpuinfo  | grep proces | wc -l)/8 + 1))"
 server="$1"
-
 
 cd_dir="cd Math/cado-nfs"
 run_cado="./cado-nfs-client.py --override t 8 --bindir=build/\$(hostname) --server=$server"
 
 echo "screen -S cado-nfs-client -t cado -d -m"
-echo "screen -S cado-nfs-client -p 0 -X stuff $'htop\\\r'"
+echo "screen -S cado-nfs-client -p 0 -X stuff \"htop^M\""
 
 for i in `seq 1 $processes`; do
     echo "screen -S cado-nfs-client -X screen -t cado-$i"
-    echo "screen -S cado-nfs-client -p $i -X stuff $'$cd_dir; time $run_cado \\\r'"
+    echo "screen -S cado-nfs-client -p $i -X stuff \"$cd_dir; time $run_cado^M\""
 done
